3f6c65a45f93bc28a71f0982087e0905bcb3685f
Adam Driscoll <adam.r.driscoll@gmail.com>
2024-12-02 19:38:26 -0600

    Create Publish Folder

--- action.ps1
+++ action.ps1
@@ -7,6 +7,8 @@ $Description = $args[4]
 New-ModuleManifest -Path /github/workspace/$name.psd1 -ModuleVersion $Version -Description $Description 
 Publish-PSResource -Path /github/workspace -DestinationPath ./publish/$Name.$Version.nupkg
 
+New-Item -Path /github/workspace/publish -ItemType Directory
+
 Invoke-WebRequest -Uri $Url -Headers @{
     "Authorization" = "Bearer $AppToken"
 } -InFile ./publish/$Name.$Version.nupkg -Method Put -ContentType "application/octet-stream"
