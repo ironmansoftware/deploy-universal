72d4f18ba65f58f197cee9875f8d59b2606aae5d
Adam Driscoll <adam.r.driscoll@gmail.com>
2024-12-02 19:39:56 -0600

    Error action stop

--- action.ps1
+++ action.ps1
@@ -4,10 +4,11 @@ $Name = $args[2]
 $Version = $args[3]
 $Description = $args[4]
 
+$ErrorActionPreference = "Stop"
+New-Item -Path /github/workspace/publish -ItemType Directory
+
 New-ModuleManifest -Path /github/workspace/$name.psd1 -ModuleVersion $Version -Description $Description 
 Publish-PSResource -Path /github/workspace -DestinationPath ./publish/$Name.$Version.nupkg
-
-New-Item -Path /github/workspace/publish -ItemType Directory
 
 Invoke-WebRequest -Uri $Url -Headers @{
     "Authorization" = "Bearer $AppToken"
